646556679087e50e85fd94ab60f4f0b20e0623ff
@bidkar <bidkar@gmail.com>
2019-09-03 08:26:30 -0600

    Practica 01
    Ejercicio inicial de c# para la configuración del editor VSCODE

---
+++ practica01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace practica01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nombre = "Bidkar";
+
+            Console.WriteLine($"Hello {nombre}!");
+        }
+    }
+}
