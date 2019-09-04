2456f35da304464c4db6cd5a9c1a94e728acefdb
@bidkar <bidkar@gmail.com>
2019-09-04 08:30:42 -0600

    Refactorización de código
    + Se optimizó el código anterior, solicitando la edad
      hasta que sea ingresada correctamente
    + TODO: Evaluar la edad para imprimir mensaje en base a ésta

--- practica02/Program.cs
+++ practica02/Program.cs
@@ -6,19 +6,26 @@ namespace practica02
     {
         static void Main(string[] args)
         {
+            int age;
+
             Console.Write("Escribe tu nombre: ");
             var name = Console.ReadLine();
+
             Console.Write("Escribe tu edad: ");
-            try
-            {
-                int age = int.Parse(Console.ReadLine());
-                Console.WriteLine($"Hello {name}, you're {age} old!");
-            }
-            catch (System.FormatException)
+            while (!int.TryParse(Console.ReadLine(), out age))
             {
                 Console.WriteLine("Error: La edad debe ser un valor numérico entero.");
+                Console.Write("Escribe tu edad: ");
             }
 
+            Console.WriteLine($"Hello {name}, you're {age} old!");
+            // TODO: De acuerdo a la edad que hayas ingresado debe imprimir
+            // 0-10 You're a child
+            // >10<18 You're a teenager
+            // >18<30 You're young
+            // >30<60 You're an adult
+            // >60<100 You're old
+            // >100 You're very close to RIP
         }
     }
 }
