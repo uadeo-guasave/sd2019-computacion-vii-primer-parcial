167b168d848b51630a59dbbb8ae81d839fe376c1
@bidkar <bidkar@gmail.com>
2019-09-04 08:00:49 -0600

    Entrada de datos por teclado
    + Ingreso de información por teclado, de tipo string y numéricos enteros
    + Se controla la excepción cuando no puede convertir de string>numérico

---
+++ practica02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace practica02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe tu nombre: ");
+            var name = Console.ReadLine();
+            Console.Write("Escribe tu edad: ");
+            try
+            {
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Hello {name}, you're {age} old!");
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Error: La edad debe ser un valor numérico entero.");
+            }
+
+        }
+    }
+}
